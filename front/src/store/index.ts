b36eeb346bb9b3531c11bede595c1edd074d5171
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2024-11-29 14:43:02 +0330

    front:: fix(custom hook -> useConversation): resolve ts type issue

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,33 +1,33 @@
-import {Message, User} from "@/types"
-import {create} from "zustand"
+import { Message, User } from "@/types";
+import { create } from "zustand";
 
 interface ConversationStoreType {
-    selectedConversation: User | null;
-    setSelectedConversation: (selectedConversation: User | null) => void;
-    //
-    messages: Array<Message>;
-    setMessages: (messages: Array<Message>) => void;
-    //
-    users: Array<User>;
-    setUsers: (users: Array<User>) => void;
-    //
-    notifications: Array<string>;
-    setNotifications: (notifications: Array<string>) => void;
+  selectedConversation: User | null;
+  setSelectedConversation: (selectedConversation: User | null) => void;
+  //
+  messages: Array<Message>;
+  setMessages: (messages: Array<Message>) => void;
+  //
+  users: Array<User>;
+  setUsers: (users: Array<User>) => void;
+  //
+  notifications: Array<string>;
+  setNotifications: (notifications: Array<string>) => void;
 }
 
-const useConversation= create<ConversationStoreType>((set) => ({
-    selectedConversation: null,
-    setSelectedConversation: (selectedConversation) => set({selectedConversation}),
-    //
-    messages: [],
-    setMessages: (messages) => set({messages}),
-    //
-    users: [],
-    setUsers: (users) => set({users}),
-    //
-    notifications: [],
-    setNotifications: (notifications) => set({notifications}),
-
-}))
+const useConversation = create<ConversationStoreType>((set) => ({
+  selectedConversation: null,
+  setSelectedConversation: (selectedConversation: User | null) =>
+    set({ selectedConversation }),
+  //
+  messages: [],
+  setMessages: (messages: Array<Message>) => set({ messages }),
+  //
+  users: [],
+  setUsers: (users: Array<User>) => set({ users }),
+  //
+  notifications: [],
+  setNotifications: (notifications: Array<string>) => set({ notifications }),
+}));
 
 export default useConversation;
