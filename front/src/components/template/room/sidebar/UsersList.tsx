7a67a71ea2a5a6a8dee30491b84eb9c62da04fae
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2024-11-30 09:57:31 +0330

    front:: fix(users state): Add the missing users data to the state

--- front/src/components/template/room/sidebar/UsersList.tsx
+++ front/src/components/template/room/sidebar/UsersList.tsx
@@ -2,49 +2,60 @@ import UserCard from "@/components/modules/room/sidebar/UserCard.tsx";
 import { ScrollArea } from "@/components/ui/scroll-area.tsx";
 import useSWR from "swr";
 import client from "@/configs/axiosRequest.ts";
-import {User} from "@/types"
-import {Skeleton} from "@/components/ui/skeleton.tsx";
-import {Frown} from "lucide-react";
+import { User } from "@/types";
+import { Skeleton } from "@/components/ui/skeleton.tsx";
+import { Frown } from "lucide-react";
+import useConversation from "@/store";
+import { useEffect } from "react";
 
 export default function UsersList() {
-      /* ---------- hook ---------- */
-    const {data: users,isLoading} = useSWR<Array<User>>("users", () => {
-        return  client.get("/api/user/all").then(res => res.data);
-    });
+  /* ---------- store ---------- */
+  const { users, setUsers } = useConversation();
 
-    if(isLoading) {
-        return (
-            <>
-                {
-                    Array.from({length: 5}).fill(undefined).map((_,idx) => (<div key={idx} className="flex items-center space-x-4">
-                        <Skeleton className="size-12 shrink-0 rounded-full"/>
-                        <div className="space-y-2 w-full">
-                            <Skeleton className="h-4"/>
-                            <Skeleton className="h-4 w-2/3"/>
-                        </div>
-                    </div>))
-                }
+  /* ---------- hook ---------- */
+  const { data, isLoading } = useSWR<Array<User>>("users", () => {
+    return client.get("/api/user/all").then((res) => res.data);
+  });
 
-            </>
-        )
-    }
+  /* ---------- life cycle ---------- */
+  useEffect(() => {
+    if (data) setUsers(data);
+  }, [data, setUsers]);
 
-    if(!users?.length) {
-        return (
-            <div className="flex-center flex-col pt-10">
-                <Frown className="text-muted-foreground size-12" />
-                <p className="text-muted-foreground text-lg font-semibold">You are alone</p>
-            </div>
-        )
-    }
-
+  if (isLoading) {
     return (
-        <ScrollArea className="h-full pr-2">
-            <div className="flex flex-col gap-4">
-                {
-                    users?.map(user => (<UserCard key={user.id} user={user}/>))
-                }
+      <>
+        {Array.from({ length: 5 })
+          .fill(undefined)
+          .map((_, idx) => (
+            <div key={idx} className="flex items-center space-x-4">
+              <Skeleton className="size-12 shrink-0 rounded-full" />
+              <div className="space-y-2 w-full">
+                <Skeleton className="h-4" />
+                <Skeleton className="h-4 w-2/3" />
+              </div>
             </div>
-        </ScrollArea>
+          ))}
+      </>
     );
+  }
+
+  if (!users?.length) {
+    return (
+      <div className="flex-center flex-col pt-10">
+        <Frown className="text-muted-foreground size-12" />
+        <p className="text-muted-foreground text-lg font-semibold">
+          You are alone
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <ScrollArea className="h-full pr-2">
+      <div className="flex flex-col gap-4">
+        {users?.map((user) => <UserCard key={user.id} user={user} />)}
+      </div>
+    </ScrollArea>
+  );
 }
