5e6a945a832df46f787f77af8ab9f56c3a44ff18
alireza <alireza.sharifinasabb@yahoo.com>
2024-12-07 20:51:43 +0330

    front:: feat(socket -> getOnlineUsers): add online users socket functionality on front-end

--- front/src/components/modules/room/sidebar/UserCard.tsx
+++ front/src/components/modules/room/sidebar/UserCard.tsx
@@ -1,35 +1,61 @@
-import {Avatar, AvatarFallback, AvatarImage,} from "@/components/ui/avatar.tsx";
-import {Card} from "@/components/ui/card.tsx";
-import {User} from "@/types"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar.tsx";
+import { Card } from "@/components/ui/card.tsx";
+import { User } from "@/types";
 import useConversation from "@/store";
-import {cn} from "@/lib/utils.ts";
+import { cn } from "@/lib/utils.ts";
+import { useSocketContext } from "@/contexts/socket.tsx";
 
-export default function UserCard({user}: {user: User}) {
-  const {selectedConversation,setSelectedConversation} = useConversation();
+export default function UserCard({ user }: { user: User }) {
+  /* ---------- hook ---------- */
+  const { selectedConversation, setSelectedConversation } = useConversation();
 
-    /* ---------- handler ---------- */
+  /* ---------- context ---------- */
+  const { onlineUsers } = useSocketContext();
+
+  /* ---------- constant ---------- */
+  const isOnline = onlineUsers?.some((item) => item === user.id);
+
+  /* ---------- handler ---------- */
   const handleSelectConversation = () => {
-    setSelectedConversation(user)
-  }
+    setSelectedConversation(user);
+  };
 
   /* ---------- constant ---------- */
   const isSelectedConversation = selectedConversation?.id === user.id;
   return (
-    <Card onClick={handleSelectConversation} className={cn("cursor-pointer group hover:shadow-stone-500", isSelectedConversation && "shadow-stone-500")}>
+    <Card
+      onClick={handleSelectConversation}
+      className={cn(
+        "cursor-pointer group hover:shadow-stone-500",
+        isSelectedConversation && "shadow-stone-500",
+      )}
+    >
       <div className="flex items-center gap-4 py-3 px-2">
-        <div className="relative online">
-          <Avatar className={cn("group-hover:scale-105 duration-100", isSelectedConversation && "scale-105")}>
-            {user.avatar && (<AvatarImage src={user.avatar} />)}
+        <div className={cn("relative", isOnline && "online")}>
+          <Avatar
+            className={cn(
+              "group-hover:scale-105 duration-100",
+              isSelectedConversation && "scale-105",
+            )}
+          >
+            {user.avatar && <AvatarImage src={user.avatar} />}
             <AvatarFallback>
-              {
-                `${user.firstName[0].toUpperCase()}${user.lastName[0].toUpperCase()}`
-              }
+              {`${user.firstName[0].toUpperCase()}${user.lastName[0].toUpperCase()}`}
             </AvatarFallback>
           </Avatar>
         </div>
 
         <div className="space-y-1">
-          <p className={cn("text-xs font-semibold group-hover:text-sm transition-all", isSelectedConversation && "font-bold text-sm text-amber-800")}>
+          <p
+            className={cn(
+              "text-xs font-semibold group-hover:text-sm transition-all",
+              isSelectedConversation && "font-bold text-sm text-amber-800",
+            )}
+          >
             {user.firstName} {user.lastName}
           </p>
           {/*<div className="flex items-center gap-1">*/}
--- front/src/contexts/socket.tsx
+++ front/src/contexts/socket.tsx
@@ -1,33 +1,60 @@
 import { io, Socket } from "socket.io-client";
-import { createContext, ReactNode, useEffect, useMemo, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { useUserContext } from "@/contexts/user.tsx";
 import { BASE_URL } from "@/helpers/constants.ts";
 
 interface SocketContextProps {
   socket: Socket | null;
+  onlineUsers: Array<string> | null;
 }
 
 const SocketContext = createContext<SocketContextProps | undefined>(undefined);
 
+export function useSocketContext() {
+  const context = useContext(SocketContext);
+  if (!context) throw new Error("socketContext not found");
+  return context;
+}
+
 export function SocketContextProvider({ children }: { children: ReactNode }) {
   /* ---------- state ---------- */
   const [socket, setSocket] = useState<Socket | null>(null);
+  const [onlineUsers, setOnlineUsers] = useState<Array<string> | null>(null);
 
   /* ---------- context ---------- */
   const { user } = useUserContext();
 
   /* ---------- hook ---------- */
-  const socketContextProviderValue = useMemo(() => ({ socket }), [socket]);
+  const socketContextProviderValue = useMemo(
+    () => ({ socket, onlineUsers }),
+    [socket, onlineUsers],
+  );
 
   /* ---------- life cycle ---------- */
   useEffect(() => {
     if (user) {
-      const _socket = io(BASE_URL);
+      const _socket = io(BASE_URL, {
+        query: {
+          userId: user.id,
+        },
+      });
       setSocket(_socket);
-      console.log(_socket);
+
+      _socket?.on("getOnlineUsers", (onlineUsers: Array<string>) => {
+        setOnlineUsers(onlineUsers);
+      });
     } else {
-      socket?.close();
-      setSocket(null);
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
     }
   }, [user]);
 
