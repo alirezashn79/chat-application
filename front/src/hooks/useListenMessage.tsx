a6c28f7a0de3fe957f2240026780451f9819404b
alireza <alireza.sharifinasabb@yahoo.com>
2024-12-12 12:00:26 +0330

    front:: fix( socket -> useListenMessage ): fix new message bug

--- front/src/hooks/useListenMessage.tsx
+++ front/src/hooks/useListenMessage.tsx
@@ -1,21 +1,23 @@
 import { useSocketContext } from "@/contexts/socket.tsx";
 import useConversation from "@/store";
-import { useEffect } from "react";
 import { Message } from "@/types";
+import { useEffect } from "react";
 
 export default function useListenMessage() {
   /* ---------- context ---------- */
   const { socket } = useSocketContext();
 
   /* ---------- store ---------- */
-  const { messages, setMessages } = useConversation();
+  const { messages, setMessages, selectedConversation } = useConversation();
 
   /* ---------- life cycle ---------- */
   useEffect(() => {
     socket?.on("newMessage", (newMessage: Message) => {
-      const cloneMessages = messages.slice();
-      cloneMessages.push(newMessage);
-      setMessages(cloneMessages);
+      if (newMessage.senderId === selectedConversation?.id) {
+        const cloneMessages = messages.slice();
+        cloneMessages.push(newMessage);
+        setMessages(cloneMessages);
+      }
     });
 
     return () => {
